ae0fe9f46731c4a39b8c1818ed7984a3b1615c17
ABastinou <101596380+BASTINOU777@users.noreply.github.com>
2023-01-10 12:55:06 +0100

    add Script AppManager 🧑‍🍳

--- Assets/Scenes/Scripts/AppManager.cs
+++ Assets/Scenes/Scripts/AppManager.cs
@@ -1,16 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class AppManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] public Array Colors[];
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
