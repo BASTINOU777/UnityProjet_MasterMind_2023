12348a9a123b482a1f2df6e0ae8fc2a9e9ae5bb5
ABastinou <101596380+BASTINOU777@users.noreply.github.com>
2023-01-11 14:01:40 +0100

    Work on fonction in Pawn Script ♟️

--- Assets/Scripts/Pawns.cs
+++ Assets/Scripts/Pawns.cs
@@ -3,40 +3,90 @@ using System.Collections.Generic;
 using System.Threading;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class Pawns : MonoBehaviour
 {
-    [SerializeField] private Collider _collider ;
-    [SerializeField] private int _actualColorNumber;
     [SerializeField] private GameObject _pawn;
 
-    //function pour trouver la position de pawn à partir du raycast de la main camera
-    //    private Vector3 RaycastPawn()
-    //    {
-    //        // new vector3 pour transmettre les position du Game object 
-    //        Vector3 PawnPos = new Vector3(_pawn.transform.position.x, _pawn.transform.position.y, _pawn.transform.position.z);
-    //        // au raycast je localise pawn 
-    //        Vector3 Direction = PawnPos - _pawn.transform.position;
-    //        // 
-    //        Debug.DrawRay(_pawn.transform.position, Direction * 20f, Color.red);
-    //        if
-    //        {
-    //        RaycastHit GetComponent<Collider>() != null
-    //        return RaycastPawn;
-    //        }
-    //}
-    //function pour afficher la couleur du pawn au over de la sourie 
+    //Récuperer la position de la souris au clik
+    void Update()
+    {
+        if (Input.GetButtonDown("MousePosition"))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            {
+                Debug.Log(mousePos.x);
+                Debug.Log(mousePos.y);
+            }
+            // si bouton gauche de la souris est detecté au clik 
+        if (Input.GetMouseButton(0))
+            {
+                Debug.Log("Pressed left click");
+             }
+
+            }
+     }
+    //fonction pour déclancher le bouton de la souris au over
     private void OnMouseOver()
     {
-
+        
     }
-
+    // fonction pour changer la couleur du pion 
     public void PawnChangeColor(Material mat)
     {
         GetComponent<MeshRenderer>().material = mat;
     }
-    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function pour trouver la position de pawn à partir du raycast de la main camera
+//    private Vector3 RaycastPawn()
+//    {
+//        // new vector3 pour transmettre les position du Game object 
+//        Vector3 PawnPos = new Vector3(_pawn.transform.position.x, _pawn.transform.position.y, _pawn.transform.position.z);
+//        // au raycast je localise pawn 
+//        Vector3 Direction = PawnPos - _pawn.transform.position;
+//        // 
+//        Debug.DrawRay(_pawn.transform.position, Direction * 20f, Color.red);
+//        if
+//        {
+//        RaycastHit GetComponent<Collider>() != null
+//        return RaycastPawn;
+//        }
+//}
+//function pour afficher la couleur du pawn au over de la sourie 
+//    private void OnMouseOver()
+//    {
+
+//    }
+
+//    public void PawnChangeColor(Material mat)
+//    {
+//        GetComponent<MeshRenderer>().material = mat;
+//    }
+
+//}
 
 
